cc1d319a50a21dadc55505a75956b700137edc53
Reginald Galang <reguv760@gmail.com>
2018-03-03 15:39:03 -0800

    minor style fixes: Ubunto Monospace for sidebarNav + portfolio h2

--- src/assets/js/script-animation.js
+++ src/assets/js/script-animation.js
@@ -1,12 +1,14 @@
 window.onload = function()
 {
   const sideNav = document.getElementsByClassName('sidebar__nav')[0];
-  //const mobileNav = document.getElementsByClassName('nav_links')[1];
+  const mobileNav = document.getElementsByClassName('mobileNav__links')[0];
   sideNav.style.opacity = "0";
   sideNav.style.left= "-10rem";
   TweenMax.to(sideNav, 0.5, {opacity:1, left:0, ease:Bounce.easeOut});
-  //mobileNav.style.opacity = "0";
-  //TweenMax.to(mobileNav, 1, {opacity:1, ease:Circ.easeOut,});
+
+  // mobileNav.style.opacity = "0";
+  // mobileNav.style.top="-20rem";
+  // TweenMax.to(mobileNav, 0.5, {top:0, opacity:1, ease:Bounce.easeOut});
 
   // var images = document.getElementsByTagName('img');
   //console.log(images.length);
