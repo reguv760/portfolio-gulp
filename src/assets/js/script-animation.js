f684549836c44fb7fa5bf974f71f022f55a66635
Reginald Galang <reguv760@gmail.com>
2018-03-10 12:43:04 -0800

    portfolio overlay added

--- src/assets/js/script-animation.js
+++ src/assets/js/script-animation.js
@@ -1,9 +1,17 @@
 window.onload = function()
 {
   const sideNav = document.getElementsByClassName('sidebar__nav')[0];
+  const sideNavFooter = document.getElementsByClassName('sidebar__footer')[0];
   const mobileNav = document.getElementsByClassName('mobileNav__links')[0];
-  sideNav.style.left= "-15rem";
+  //sideNavFooter.style.opacity = "0";
+  //sideNavFooter.style.bottom     = "1.25rem"
+  sideNav.style.left          = "-15rem";
+
   TweenMax.to(sideNav, 0.6, {delay:0.25,left:0, ease:Bounce.easeOut});
+  // TweenMax.to(sideNav, 0.6, {delay:0.25,left:0,  ease:Bounce.easeOut, onComplete: function()
+  // {
+  //   TweenMax.to(sideNavFooter, 0.5, {opacity:1, bottom:"1rem", ease:Circ.easeOut});
+  // }});
 
   // mobileNav.style.opacity = "0";
   // mobileNav.style.top="-20rem";
@@ -19,6 +27,43 @@ window.onload = function()
   //     console.log(images[i]).src;
   //   }
   // }
+
+  const portOverlay = document.getElementsByClassName('portfolio__overlay'); 
+  const portOverlayCount = portOverlay.length;
+  
+  //const portImage = document.getElementsByClassName('portfolio__website-container--hero');
+
+  for (var i = 0; i < portOverlay.length; i++ )
+  {
+    //console.log(i);
+    //portOverlay[i].style.top = "-80rem";
+    portOverlay[i].style.opacity = 0;
+
+    portOverlay[i].addEventListener("mouseenter", function( event ) 
+    {     
+      var textContainer = this.getElementsByClassName("portfolio__overlay-container")[0];
+      //console.log(textContainer);
+      textContainer.style.transform = "scaleX(0)";
+
+      //TweenMax.to(event.target, 0.25, {opacity:1, ease:Power1.easeOut});
+      TweenMax.to(event.target, 0.2, {opacity:1, ease:Power1.easeOut});
+      TweenMax.to(textContainer, 0.2, {scaleX:1, delay:0.1, transformOrigin:"50% 50%", ease:Power1.easeOut});   
+    });
+
+    portOverlay[i].addEventListener("mouseleave", function( event ) 
+    {
+      var textContainer = this.getElementsByClassName("portfolio__overlay-container")[0];
+      TweenMax.to(event.target, 0.25, {opacity:0, ease:Power1.easeOut});
+      TweenMax.to(textContainer, 0.2, {scaleX:0, transformOrigin:"50% 50%", ease:Power1.easeOut});
+    });   
+    //portImage[i].mouseover(hoverPortImage);
+  }
+
+  //const portImage = document.getElementsByClassName('portfolio__website-container--hero');
+  //portImage.hover
+  
+  //portOverlay.style.opacity = "0";
+  console.log(portOverlayCount);
 
   lazyload();
 
