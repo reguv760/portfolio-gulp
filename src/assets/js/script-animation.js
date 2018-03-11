6884e78d5f9010adbc07a3edfc64df492810de31
Reginald Galang <reguv760@gmail.com>
2018-03-11 01:54:22 -0800

    portfolio overlay + mobileNav fixes

--- src/assets/js/script-animation.js
+++ src/assets/js/script-animation.js
@@ -8,30 +8,9 @@ window.onload = function()
   sideNav.style.left          = "-15rem";
 
   TweenMax.to(sideNav, 0.6, {delay:0.25,left:0, ease:Bounce.easeOut});
-  // TweenMax.to(sideNav, 0.6, {delay:0.25,left:0,  ease:Bounce.easeOut, onComplete: function()
-  // {
-  //   TweenMax.to(sideNavFooter, 0.5, {opacity:1, bottom:"1rem", ease:Circ.easeOut});
-  // }});
-
-  // mobileNav.style.opacity = "0";
-  // mobileNav.style.top="-20rem";
-  // TweenMax.to(mobileNav, 0.5, {top:0, opacity:1, ease:Bounce.easeOut});
-
-  // var images = document.getElementsByTagName('img');
-  //console.log(images.length);
-  // for (var i = 0; i < images.length; i++)
-  // {
-  //   console.log(images[i].src);
-  //   if (getFileExtension(images[i].src) === "jpg")
-  //   {
-  //     console.log(images[i]).src;
-  //   }
-  // }
 
   const portOverlay = document.getElementsByClassName('portfolio__overlay'); 
-  const portOverlayCount = portOverlay.length;
   
-  //const portImage = document.getElementsByClassName('portfolio__website-container--hero');
 
   for (var i = 0; i < portOverlay.length; i++ )
   {
@@ -42,13 +21,9 @@ window.onload = function()
     {     
       var textContainer = this.getElementsByClassName("portfolio__overlay-container")[0];
 
-      TweenMax.to(event.target, 0.25, {opacity:1, onStart: scaleTextContainer, ease:Power2.easeOut});
+      TweenMax.to(event.target, 0.25, {opacity:1, ease:Power2.easeOut});
       TweenMax.to(textContainer, 0.3, {scaleX:1, delay:0.2, transformOrigin:"50% 50%", ease:Power2.easeOut});
 
-      function scaleTextContainer()
-      {
-        textContainer.style.transform = "scaleX(0)";
-      }
     });
 
     portOverlay[i].addEventListener("mouseleave", function( event ) 
@@ -58,8 +33,6 @@ window.onload = function()
       TweenMax.to(textContainer, 0.2, {scaleX:0, ease:Power1.easeOut});
     });   
   }
-
-  console.log(portOverlayCount);
 
   lazyload();
 
